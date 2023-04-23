50e89ac8e5fe82ed18cb83048f91cf7a9effaef2
Hesham Khaled <103784580+hesham060@users.noreply.github.com>
2023-04-23 23:45:35 +0300

    v.45.done

--- auth/login.php
+++ auth/login.php
@@ -1,10 +1,16 @@
+
 <?php
 
 include "../connect.php";
- 
+
 $password = sha1("password");
-$email = filterRequest("email"); 
-$stmt = $con->prepare("SELECT * FROM users WHERE users_email = ? AND  users_password = ? AND users_approve = 1  ");
+$email = filterRequest("email");
+
+
+$stmt = $con->prepare("SELECT * FROM users WHERE users_email = ? OR users_password = ? ");
 $stmt->execute(array($email, $password));
 $count = $stmt->rowCount();
-result($count) ; 
+
+
+
+result($count);
